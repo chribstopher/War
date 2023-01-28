e37f30bc941f9e241b328f4b341caf0718033507
chribstopher <113141574+chribstopher@users.noreply.github.com>
2023-01-28 14:52:19 -0500

    Add files via upload

---
+++ War/src/war/Pile.java
@@ -0,0 +1,22 @@
+package war;
+
+import java.util.ArrayList;
+
+public class Pile {
+    public Pile (String name){}
+    public void addCard(Card card){}
+    public void clear(){}
+    public Card drawCard(boolean faceup){
+        return null;
+    }
+    public ArrayList<Card>  getCards(){
+        return null;
+    }
+    public boolean hasCard(){
+        return false;
+    }
+    public static void setSeed(long seed){}
+    public void shuffle(){}
+    @Override
+    public String toString(){return null;}
+}
---
+++ War/src/war/Player.java
@@ -0,0 +1,14 @@
+package war;
+
+public class Player {
+    public Player(int id){}
+    public void addCard(Card card){}
+    public void addCards(Pile cards){}
+    public Card drawCard(){return null;}
+    public boolean hasCard(){return false;}
+    public boolean isWinner(){return false;}
+    public void setWinner(){}
+
+    @Override
+    public String toString(){return null;}
+}
